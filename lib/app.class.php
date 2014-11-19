9ff34eab9a6c5c54fd70bd0a4374051b805343b5
Stephen Foo <stephen.frx@gmail.com>
2014-11-20 00:19:04 +0800

    Add some functions in App Class

--- lib/app.class.php
+++ lib/app.class.php
@@ -21,4 +21,37 @@ class App {
 	 * 构造函数
 	 */
 	public function __construct() {}
+
+	public function get($key) {
+		$keys = explode('.', $key);
+		return $_settings[$key];
+	}
+
+	/**
+	 * 输出错误信息
+	 * @param  string $code    错误代码
+	 * @param  string $message 错误信息
+	 * @return NULL
+	 */
+	public function error($code, $message = '') {
+		print 'ERROR: [' . $code . '] ' . $message;
+	}
+
+	/**
+	 * 计算字符串散列值
+	 * @param  string $string 字符串
+	 * @return string         散列值
+	 */
+	public function hash($string) {
+		return hash('sha1', $string);
+	}
+
+	/**
+	 * 页面重定向
+	 * @param  string $url URL地址
+	 * @return NULL
+	 */
+	public function redirect($url) {
+		header('Location: ' . $url);
+	}
 }
