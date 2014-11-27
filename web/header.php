9e78c17d57f348df7d226f5b59a9f92dbc65458b
Stephen Foo <stephen.frx@gmail.com>
2014-11-27 10:41:36 +0800

    Delete file db.class.php.old and base.class.php

--- web/header.php
+++ web/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="广西师范大学学生选课">
+        <meta name="description" content="广西师范大学学生选课系统">
         <meta name="keywords" content="广西师范大学,教务处,学生选课">
         <meta name="author" content="Fu Rongxin,符荣鑫">
         <title>广西师范大学教务管理系统 - 选课系统</title>
