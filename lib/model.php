777fadd31bb65556af919da5a41182fb00c7f442
Stephen Foo <stephen.frx@gmail.com>
2015-02-25 15:53:05 +0800

    Add Model

---
+++ lib/model.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 模型类
+ */
+class Model {
+
+	/**
+	 * 模型类对应的数据库句柄
+	 * @var object
+	 */
+	protected $db = null;
+
+	/**
+	 * 模型类控制方法
+	 */
+	public function __construct() {
+		$this->db = DB::getInstance();
+	}
+}
