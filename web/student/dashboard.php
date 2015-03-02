76015540a833c05d35a63afd41f34c5e2725ffa6
Stephen Foo <stephen.frx@gmail.com>
2015-03-02 14:25:21 +0800

    Modify student dashboard

--- web/student/dashboard.php
+++ web/student/dashboard.php
@@ -3,3 +3,16 @@
             <h1 class="page-header">综合管理系统</h1>
         </div>
     </section>
+
+    <section class="row">
+        <div class="col-lg-12">
+        	<div class="panel panel-default">
+        		<div class="panel-body">
+                    <ol>
+                        <li><?php echo $message ?></li>
+                        <li>考试报名时间：</li>
+                    </ol>
+        		</div>
+        	</div>
+        </div>
+    </section>
