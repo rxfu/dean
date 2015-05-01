924f1045248d51fe8a2f19201308c07d3ccad42b
Stephen Foo <stephen.frx@gmail.com>
2015-05-01 17:35:03 +0800

    Modify monitor initialize and Add exec method in database

--- model/MonitorModel.php
+++ model/MonitorModel.php
@@ -47,49 +47,113 @@ class MonitorModel extends ManagerAdminModel {
 
 	public function init($year,$term) {
 		//创建课程评教得分总表
- $sql = 'TRUNCATE kctable';
- $this->db->query($sql);
- $sql = 'TRUNCATE kcxhtable';
- $this->db->query($sql);
- $sql = 'TRUNCATE xkxxtable';
- $this->db->query($sql);
+ $sql = 'TRUNCATE kctable, kcxhtable, xkxxtable';
+ $this->db->delete($sql);
  //kcxhTable 临时表 记录各门课程序号的实评人数
- mysql_query("insert into jxpg.kcxhtable select distinct c_kcxh,c_kcbh,count(*)/10 from jxpg.t_zl_xspf where c_nd='$ND' and c_xq='$XQ' group by c_kcxh");
+ $sql = 'INSERT INTO kcxhtable SELECT DISTINCT c_kcxh, c_kcbh, COUNT(*) / 10 FROM t_zl_xspf WHERE c_nd = ? AND c_xq = ? GROUP BY c_kcxh';
+ $this->db->insert($sql, array($year, $term));
 //kctable 临时表 记录某课程序号所有指标下各评分等级的人数
- mysql_query("insert into jxpg.kctable SELECT c_kcxh,c_xm,rank_id,count(*) FROM jxpg.t_zl_xspf where c_nd = '$ND' and c_xq ='$XQ' GROUP BY c_kcxh ,c_xm, rank_id");
+ $sql = 'INSERT INTO kctable SELECT c_kcxh, c_xm, rank_id, COUNT(*) FROM t_zl_xspf WHERE c_nd = ? and c_xq = ? GROUP BY c_kcxh, c_xm, rank_id';
+ $this->db->insert($sql, array($year,$term));
 //xkxxtable 临时表 记录某课程的选课人数，也即课程的应评人数 
- mysql_query("insert into jxpg.xkxxtable select c_kcxh,count(*) FROM jwxt.t_xk_xkxx where c_nd='$ND' and c_xq='$XQ' group by c_kcxh ");
+ $sql = 'INSERT INTO xkxxtable SELECT c_kcxh, COUNT(*) FROM t_xk_xkxx WHERE c_nd = ? and c_xq = ? GROUP BY c_kcxh';
+ $this->db->insert($sql, array($year,$term));
 //detailTable 存入二级指标得分 每个课程序号有10条记录
- $detailTable=$ND.$XQ."Mark"; 
- $sql1 ="create table jxpg.$detailTable(
-       c_kcxh varchar(10) not null,
-	   c_kcbh varchar(10) not null,
-	   c_jsgh varchar(6) not null,
-	   c_xm char(2) not null,
-	   s_mark float(5,2)
-    )";
- mysql_query($sql1);
+ $detailTable = $year.$term."Mark"; 
+ $sql = "CREATE TABLE $detailTable(
+	       c_kcxh VARCHAR(10) NOT NULL,
+		   c_kcbh VARCHAR(10) NOT NULL,
+		   c_jsgh VARCHAR(6) NOT NULL,
+		   c_xm CHAR(2) NOT NULL,
+		   s_mark NUMERIC(5,2)
+    	)";
+ $this->db->exec($sql);
 	
- $tablename=$ND.$XQ."t"; 
- $sql ="create table jxpg.$tablename(
-       c_kcbh varchar(10) not null,
-       c_kcxh varchar(10) not null,
-	   c_kcxz char(2),
-       c_kcyx varchar(20) not null,
-	   c_skzy varchar(40) not null,
-	   c_jsgh char(10) not null,
-	   c_jsyx char(20),   
-	   s_jxtd float(5,2),
-	   s_jxnr float(5,2),	  
-	   s_jxff float(5,2),
-	   s_jxxg float(5,2),	  
-	   s_zhpf float(5,2),
-	   s_cprs int(4),
-	   s_sprs int(4)
-    )";
-   mysql_query($sql) or die("数据表已经存在! 请先删除后再创建!"); 
-   mysql_query("create index c_kcxh ON $tablename(c_kcxh(10))");
-   include "config.inc.php";
+ $tablename=$year.$term."t"; 
+ $sql = "CREATE TABLE $tablename(
+	       c_kcbh VARCHAR(10) NOT NULL,
+	       c_kcxh VARCHAR(10) NOT NULL,
+		   c_kcxz CHAR(2),
+	       c_kcyx VARCHAR(20) NOT NULL,
+		   c_skzy VARCHAR(40) NOT NULL,
+		   c_jsgh CHAR(10) NOT NULL,
+		   c_jsyx CHAR(20),   
+		   s_jxtd NUMERIC(5,2),
+		   s_jxnr NUMERIC(5,2),	  
+		   s_jxff NUMERIC(5,2),
+		   s_jxxg NUMERIC(5,2),	  
+		   s_zhpf NUMERIC(5,2),
+		   s_cprs INTEGER(4),
+		   s_sprs INTEGER(4)
+    	)";
+$rowCount=$this->db->exec($sql);
+   if (0 === $rowCount) {
+   	return false;
+   }
+
+   $sql = "CREATE INDEX c_kcxh ON $tablename(c_kcxh(10))";
+   $this->db->exec($sql);
+
+   $sql = 'SELECT c_kcxh, c_kcbh, s_sprs FROM kcxhtable';
+   $data = $this->db->search($sql);
+   foreach ($data as $row) {   	
+	 $kcxh=$row[0];                         //课程序号   
+	 $kcxz=substr($row[0],0,2);             //课程性质编号 
+	 $sprs=$row[2];                         //实评人数     
+	 
+	 $nnn=array();                          //nnn[i][j][k]统计第i个一级指标下的第j 个二级指标评分等级为k的学生人数
+	 for($i=1;$i<=$yjzb_num;$i++){
+        for($j=1;$j<=$ejzb_num[$i];$j++){
+		   $ejzb=$ejzb_id[$i][$j];
+		   $sql = 'SELECT rank_id, num FROM kctable WHERE c_kcxh = ? AND c_xm = ?';
+		   $ranks = $this->db->search($sql, array($kcxh,$ejzb));
+		   foreach ($ranks as $rank) {
+		   	$nnn[$i][$j][$rank[0]]=$rank[1];
+		   }
+	    }
+	}
+
+	$sql = 'SELECT DISTINCT c_jsgh FROM t_pk_kb WHERE c_kcxh = ? and c_nd = ? and c_xq = ?';
+	$jsghRec = $this->db->getRow($sql,array($row[0], $year,$term));
+	$jsgh=$jsghRec[0];         //教师工号 
+	$kcbh=$row[1];             //课程编号      
+
+	$f=array();                                //存放一级指标得分      
+    $f_f=0.0;                                  //该门课程的最终评分
+    for($i=1;$i<=$yjzb_num;$i++){
+      $f[$i]=0;
+      for($j=1;$j<=$ejzb_num[$i];$j++){
+	     $ejzb=$ejzb_id[$i][$j];
+	     $sum=0;
+		 for($k=1;$k<=$rank_num;$k++){
+		   $sum=$sum+$zb_rank[$k]*$nnn[$i][$j][$k];
+		 }
+		 if($sprs>0) $ff=$sum/$sprs;
+		 else $ff=0;
+		 $mark=$ff;
+		 mysql_query("insert into jxpg.$detailtable(c_kcxh,c_kcbh,c_jsgh,c_xm,s_mark) values('$kcxh','$kcbh','$jsgh','$ejzb','$mark')");
+		 $f[$i]=$f[$i]+$ejzb_qz[$i][$j]*$ff;
+	  }
+	  $f_f=$f_f+$yjzb_qz[$i]*$f[$i];
+	}
+	 $cprs_res=mysql_query("SELECT s_cprs FROM jxpg.xkxxtable where c_kcxh='$all_row[0]'",$db);
+	 $cprs_row=mysql_fetch_row($cprs_res);
+	 $jsyx_res=mysql_query("select distinct c_mc from jwxt.t_xt_yxbh,jwxt.t_pk_js1 where t_xt_yxbh.c_xb=c_yx and c_jsgh='$jsgh_row[0]'",$db);
+	 $jsyx_row=mysql_fetch_row($jsyx_res);
+	 $res_skzy=mysql_query("SELECT distinct c_zyh,c_nj,c_mc FROM jwxt.t_xt_yxbh, jwxt.t_xt_yxzy, jwxt.t_pk_kb WHERE c_xb = c_yx AND c_zy = c_zyh and c_kcxh='$all_row[0]' and c_nd='$ND' and c_xq='$XQ' ",$db);
+	 $row_skzy=mysql_fetch_row($res_skzy);
+	 $res_skmc=mysql_query("select c_mc from jwxt.t_xt_zybh where c_zy='$row_skzy[0]'",$db);
+	 $row_skmc=mysql_fetch_row($res_skmc);
+
+	 $JSYX=$jsyx_row[0];         //教师院系
+	 $CPRS=$cprs_row[0];         //课程应评人数(即选课人数)
+	                          
+	 $SKZY=$row_skzy[1]."级".$row_skmc[0];     //授课年级、专业
+	 $KCYX=$row_skzy[2];                       //课程开设院系
+   	 $sql2="insert into jxpg.$tablename(c_kcbh,c_kcxh,c_kcxz,c_kcyx,c_skzy,c_jsgh,c_jsyx,s_jxtd,s_jxnr,s_jxff,s_jxxg,s_zhpf,s_cprs,s_sprs) values('$KCBH','$KCXH','$KCXZ','$KCYX','$SKZY','$JSGH','$JSYX','$F[1]','$F[2]','$F[3]','$F[4]','$F_F','$CPRS','$all_row[2]')";
+	 mysql_query($sql2) or die(mysql_error()."  插入记录失败!");
+	 $ALL.next;
+   }
    $ALL=mysql_query("select c_kcxh,c_kcbh,s_sprs from jxpg.kcxhtable",$db);
 	while($all_row=mysql_fetch_row($ALL))
 	{
