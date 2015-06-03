c9e2ed6eb52af1f3924fb99ca0cecc3586f03c3d
Stephen Foo <stephen.frx@gmail.com>
2015-06-03 16:55:04 +0800

    Add teacher assess model

---
+++ model/AssessModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 教师评教结果模型类
+ *
+ * @author Fu Rongxin <rxfu@mailbox.gxnu.edu.cn>
+ */
+class AssessModel extends TeacherAdminModel {
+
+	public function __construct() {
+		parent::__construct();
+	}
+	
+}
