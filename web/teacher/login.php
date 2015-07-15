95248ff2632b2d6abbf1cc6e57099aa9b0dbd2a8
Stephen Foo <stephen.frx@gmail.com>
2015-07-15 16:36:22 +0800

    Add 360 browser alert

--- web/teacher/login.php
+++ web/teacher/login.php
@@ -24,6 +24,11 @@
                                                 <button type="submit" class="btn btn-lg btn-success btn-block">登录</button>
                                             </div>
                                         </div>
+                                        <div class="form-group">
+                                            <div class="col-md-12">
+                                                <div class="alert alert-warning" role="alert"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> <strong>注意！</strong>请检查自己是否正在使用<strong>360浏览器</strong>。部分360浏览器在成绩提交时会导致<strong>“提交失败”</strong>的错误，请使用IE9+、Firefox、Google Chrome、Opera等现代浏览器录入成绩。</div>
+                                            </div>
+                                        </div>
                                     </fieldset>
                                 </form>
                             </div>
